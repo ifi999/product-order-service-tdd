7b3b627241ac8a6b8af43d291e79437ea195d789
ifi999 <kkwkjm4231@gmail.com>
2023-01-27 20:37:54 +0900

    product-order-service-tdd first commit

---
+++ src/main/java/com/example/productorderservicetdd/ProductOrderServiceTddApplication.java
@@ -0,0 +1,13 @@
+package com.example.productorderservicetdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductOrderServiceTddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductOrderServiceTddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/productorderservicetdd/ProductOrderServiceTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productorderservicetdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductOrderServiceTddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

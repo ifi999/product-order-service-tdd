18fd1c4bfe0361a4a0536f19a38cd458af420eb3
ifi999 <kkwkjm4231@gmail.com>
2023-01-29 22:44:07 +0900

    add: JPA 적용

--- src/main/java/com/example/productorderservicetdd/product/Product.java
+++ src/main/java/com/example/productorderservicetdd/product/Product.java
@@ -1,8 +1,19 @@
 package com.example.productorderservicetdd.product;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.util.Assert;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "products")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private int price;
@@ -16,25 +27,5 @@ class Product {
         this.name = name;
         this.price = price;
         this.discountPolicy = discountPolicy;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public DiscountPolicy getDiscountPolicy() {
-        return discountPolicy;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void assignId(final Long aLong) {
-        this.id = id;
     }
 }
